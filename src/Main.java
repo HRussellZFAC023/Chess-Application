dfbcb4ac43a2c894a919b035aeaf2980ec61ab4d
Henry Russell <hrussell.63250@farnborough.ac.uk>
2017-10-13 10:34:06 +0100

    Now we have a fancy icon

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
@@ -22,7 +23,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-
+        stage.getIcons().add(new Image("chess-33-xxl.png"));
 
         VBox root = new VBox();
         Scene scene = new Scene(root, 1024, 768);
@@ -31,49 +32,44 @@ public class Main extends Application {
 
         MenuBar myMenu = new MenuBar();
 
-        Menu numbersMenu = new Menu("Numbers");
-        MenuItem numbersItem1 = new MenuItem("One");
-        MenuItem numbersItem2 = new MenuItem("Two");
-        MenuItem numbersItem3 = new MenuItem("Three");
-        numbersMenu.getItems().addAll(numbersItem1, numbersItem2, numbersItem3);
+        Menu gameMenu = new Menu("Game");
+        MenuItem gameItem1 = new MenuItem("New Game");
+        MenuItem gameItem2 = new MenuItem("Save");
+        MenuItem gameItem3 = new MenuItem("Open");
+        MenuItem gameItem4 = new MenuItem("Setup Position");
+        MenuItem gameItem5 = new MenuItem("Quit");
+        gameMenu.getItems().addAll(gameItem1, gameItem2, gameItem3, gameItem4, gameItem5);
 
-        Menu coloursMenu = new Menu("Colours");
-        MenuItem coloursItem1 = new MenuItem("Red");
-        MenuItem coloursItem2 = new MenuItem("Green");
-        MenuItem coloursItem3 = new MenuItem("Blue");
-        coloursMenu.getItems().addAll(coloursItem1, coloursItem2, coloursItem3);
+        Menu tournamentMenu = new Menu("Tournament");
+        MenuItem tournamentItem1 = new MenuItem("Generate Tournament");
+        MenuItem tournamentItem2 = new MenuItem("Load Tournament");
+        tournamentMenu.getItems().addAll(tournamentItem1, tournamentItem2);
 
-        Menu shapesMenu = new Menu("Shapes");
-        MenuItem shapesItem1 = new MenuItem("Triangle");
-        MenuItem shapesItem2 = new MenuItem("Square");
-        MenuItem shapesItem3 = new MenuItem("Circle");
-        shapesMenu.getItems().addAll(shapesItem1, shapesItem2, shapesItem3);
+        Menu trainingMenu = new Menu("Training");
+        MenuItem trainingItem1 = new MenuItem("New puzzle");
+        MenuItem trainingItem2 = new MenuItem("Create puzzle");
+        trainingMenu.getItems().addAll(trainingItem1, trainingItem2 );
 
-        myMenu.getMenus().addAll(numbersMenu, coloursMenu, shapesMenu);
+        Menu aboutMenu = new Menu("About");
+        MenuItem aboutItem1 = new MenuItem("Rules of chess");
+        MenuItem aboutItem2 = new MenuItem("Credits");
+        aboutMenu.getItems().addAll(aboutItem1, aboutItem2 );
+
+        myMenu.getMenus().addAll(gameMenu, trainingMenu, aboutMenu);
         root.getChildren().add(myMenu);
 
         BorderPane borderPane = new BorderPane();
         borderPane.prefHeightProperty().bind(root.heightProperty());
         root.getChildren().add(borderPane);
-        borderPane.setPadding(new Insets(40, 40, 40, 40));
+        borderPane.setPadding(new Insets(40));
 
         stage.setTitle("Chess Application");
-        Button myButton = new Button("Click me!");
 
-        myButton.setOnAction((ActionEvent ae) -> doSomething(ae));
 
-        borderPane.getChildren().add(myButton);
 
-        GridPane leftPane = new GridPane();
-        Button leftButton1 = new Button("I am left.");
-        leftPane.getChildren().add(leftButton1);
-        Button leftButton2 = new Button("I am left again.");
-        leftPane.getChildren().add(leftButton2);
-        borderPane.setLeft(leftPane);
-        leftPane.setAlignment(Pos.CENTER);
-        BorderPane.setAlignment(leftPane, Pos.CENTER_LEFT);
 
-        GridPane rightPane = new GridPane();
+
+       VBox rightPane = new VBox();
         Button rightButton1 = new Button("I am right.");
         rightPane.getChildren().add(rightButton1);
         Button rightButton2 = new Button("I am right again.");
@@ -82,26 +78,10 @@ public class Main extends Application {
         rightPane.setAlignment(Pos.CENTER);
         BorderPane.setAlignment(rightPane, Pos.CENTER_RIGHT);
 
-        GridPane topPane = new GridPane();
-        Button topButton1 = new Button("I am top.");
-        topPane.getChildren().add(topButton1);
-        Button topButton2 = new Button("I am top again.");
-        topPane.getChildren().add(topButton2);
-        borderPane.setTop(topPane);
-        topPane.setAlignment(Pos.CENTER);
-        BorderPane.setAlignment(topPane, Pos.TOP_CENTER);
-
-        GridPane bottomPane = new GridPane();
-
-
-        borderPane.setBottom(bottomPane);
-        bottomPane.setAlignment(Pos.CENTER);
-        BorderPane.setAlignment(bottomPane, Pos.BOTTOM_CENTER);
-
-
         GridPane centerPane = new GridPane();
 
         boolean light;
+        boolean selected = false;
         int ascii = 65;
 
 
@@ -109,7 +89,7 @@ public class Main extends Application {
             for (int y = 0; y < 8; y++) {
                 Button chessSpace = new Button(/*Integer.toString(x) + ", " + Integer.toString(y)*/);
                 chessSpace.getStyleClass().add("chess-space");
-                chessSpace.setOnAction((ActionEvent ae) -> doSomething(ae));
+                chessSpace.setOnAction((ActionEvent ae) -> doSomething(ae,selected));
                 light = ((x + y) % 2 == 0 );
                 if(light) {
                     chessSpace.getStyleClass().add("chess-space-light");
@@ -132,16 +112,28 @@ public class Main extends Application {
                 new Moves("d4", "e5xd4")
         );
         TableView table = new TableView<>();
-        table.setPrefSize(150, 250);
+        table.setPrefSize(200, 200);
         table.setItems(moves);
+
 
         TableColumn whiteMoves = new TableColumn<>("White");
         whiteMoves.setCellValueFactory(new PropertyValueFactory<>("White"));
         table.getColumns().add(whiteMoves);
+        whiteMoves.setSortable(false);
 
         TableColumn blackMoves = new TableColumn<>("Black");
         blackMoves.setCellValueFactory(new PropertyValueFactory<>("Black"));
         table.getColumns().add(blackMoves);
+        blackMoves.setSortable(false);
+
+        whiteMoves.prefWidthProperty().bind(table.widthProperty().multiply(0.5));
+        blackMoves.prefWidthProperty().bind(table.widthProperty().multiply(0.5));
+
+        whiteMoves.setResizable(false);
+        blackMoves.setResizable(false);
+        table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
+
 
 
         borderPane.setRight(table);
@@ -170,9 +162,8 @@ public class Main extends Application {
     }
 
 
-    private Object doSomething(ActionEvent ae) {
-        //chessSpace
-        return null;
+    private Object doSomething(ActionEvent ae, boolean selected) {
+        return(!selected);
     }
 
     public static void main(String[] args) {
